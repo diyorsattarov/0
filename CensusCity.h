30a16f000fd02e1e17ac3550698096a1de625414
isa <38271173+isaas7@users.noreply.github.com>
2024-10-08 15:32:48 -0500

    Update CensusCity.h
    Signed-off-by: isa <38271173+isaas7@users.noreply.github.com>

--- CensusCity.h
+++ CensusCity.h
@@ -1,39 +1,39 @@
 #pragma once
 #include <string>
 
-class CensusCity
+class A
 {
-	std::string city;
-	std::string state;
-	int census2010;
-	int estimate2011;
-	bool use2011;
+    std::string a1;
+    std::string a2;
+    int a3;
+    int a4;
+    bool a5;
 
 public:
-	CensusCity() :city(""), state(""), census2010(0), estimate2011(0), use2011(false) {};
-	CensusCity(std::string aCity, std::string aState, int n1, int n2, bool tf) :city(aCity), state(aState), census2010(n1), estimate2011(n2), use2011(tf) {};
-	~CensusCity() {};
+    A() : a1(""), a2(""), a3(0), a4(0), a5(false) {};
+    A(std::string b1, std::string b2, int n1, int n2, bool c) : a1(b1), a2(b2), a3(n1), a4(n2), a5(c) {};
+    ~A() {};
 
-	std::string toString() { return city + ", " + state + " (" + std::to_string(census2010) + ", " + std::to_string(estimate2011) + ")"; }
+    std::string b() { return a1 + ", " + a2 + " (" + std::to_string(a3) + ", " + std::to_string(a4) + ")"; }
 
-	friend int compare(const CensusCity& c1, const CensusCity& c2);
+    friend int c(const A& d1, const A& d2);
 };
 
-int compare(const CensusCity& c1, const CensusCity& c2)
+int c(const A& d1, const A& d2)
 {
-	if (c1.use2011)
-	{
-		if (c1.estimate2011 < c2.estimate2011)
-			return -1;
-		if (c1.estimate2011 > c2.estimate2011)
-			return 1;
-		return 0;
-	}
-	else {
-		if (c1.census2010 < c2.census2010)
-			return -1;
-		if (c1.census2010 > c2.census2010)
-			return 1;
-		return 0;
-	}
+    if (d1.a5)
+    {
+        if (d1.a4 < d2.a4)
+            return -1;
+        if (d1.a4 > d2.a4)
+            return 1;
+        return 0;
+    }
+    else {
+        if (d1.a3 < d2.a3)
+            return -1;
+        if (d1.a3 > d2.a3)
+            return 1;
+        return 0;
+    }
 }
